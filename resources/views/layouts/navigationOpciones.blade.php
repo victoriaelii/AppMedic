73fc49546350d6751a7417159aa787d8c1e017f1
victoriaelii <2130044@upv.edu.mx>
2024-06-17 00:55:39 -0600

    Vicky, DOM12

--- resources/views/layouts/navigationOpciones.blade.php
+++ resources/views/layouts/navigationOpciones.blade.php
@@ -23,7 +23,7 @@
                 <!-- Enlace a Médicos -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('medicos')" :active="request()->routeIs('medicos')">
-                        {{ __('Médicos') }}
+                        {{ __('Usuarios') }}
                     </x-nav-link>
                 </div>
 
@@ -120,7 +120,7 @@
         <!-- Enlace a Médicos (versión responsiva) -->
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('medicos')" :active="request()->routeIs('medicos')">
-                {{ __('Médicos') }}
+                {{ __('Usuarios') }}
             </x-responsive-nav-link>
         </div>
 
